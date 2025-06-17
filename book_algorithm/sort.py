ce6f4c73daa1558f4942f18afd1b5428460a3851
xxxgqcoder <xxxgqcoder@gmail.com>
2025-06-17 10:27:29 +0800

    add comment

--- book_algorithm/sort.py
+++ book_algorithm/sort.py
@@ -1,3 +1,4 @@
+# insert sort
 def insert_sort(a):
     for j in range(1, len(a)):
         # assume a[0, ..., j-1] is sorted
@@ -26,15 +27,21 @@ def insert_sort_v2(a):
         a[j] = tmp
 
 
+# quick sort
 def median3(a, left, right):
-    center = (left + right) // 2
-    if a[center] < a[left]:
-        a[left], a[center] = a[center], a[left]
+    """
+    Find pivot value from left, middle and right position of a.
+    """
+    # sort left, mid, right
+    mid = (left + right) // 2
+    if a[mid] < a[left]:
+        a[left], a[mid] = a[mid], a[left]
     if a[right] < a[left]:
         a[right], a[left] = a[left], a[right]
-    if a[right] < a[center]:
-        a[right], a[center] = a[center], a[right]
-    a[center], a[right - 1] = a[right - 1], a[center]
+    if a[right] < a[mid]:
+        a[mid], a[right] = a[right], a[mid]
+    # put a[mid] to right-1 and return a[right-1] as pivot value
+    a[mid], a[right - 1] = a[right - 1], a[mid]
     return a[right - 1]
 
 
@@ -42,30 +49,34 @@ def partition(a, left, right):
     pivot = median3(a, left, right)
     if right - left <= 2:
         return (left + right) // 2
+
+    # initially a[j] = pivot, since pivot is put at right - 1
     i, j = left, right - 1
     while i < j:
         while True:
-            i += 1  # at least move forward one step, in case a[i] = pivot
-            if a[i] >= pivot:
+            i += 1
+            if a[i] >= pivot:  # i will at most stop at right - 1, since a[right-1] = pivot after median3
                 break
         while True:
-            j -= 1  # at least move backward one step, in case a[j] = pivot
-            if a[j] <= pivot:
+            j -= 1
+            if a[j] <= pivot:  # j will at least stop at left, as a[left] <= a[right-1] after median3
                 break
         if i < j:
             a[i], a[j] = a[j], a[i]
+    # a[i] >= pivot
     a[i], a[right - 1] = a[right - 1], a[i]
     return i
 
 
 def quick_sort(a, left, right):
-    if len(a) <= 1 or left >= right:
+    if left >= right:
         return
     i = partition(a, left, right)
     quick_sort(a, left, i - 1)
     quick_sort(a, i + 1, right)
 
 
+# quick sort v2
 def partition_v2(a, p, r):
     x = a[r]
     i = p - 1
