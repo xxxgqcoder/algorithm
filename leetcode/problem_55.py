8c72f734f93eb3d9821899b9585ab890fb861a55
xxxgqcoder <xxxgqcoder@gmail.com>
2025-06-19 13:45:02 +0800

    other method

--- leetcode/problem_55.py
+++ leetcode/problem_55.py
@@ -13,6 +13,33 @@ class Solution:
 
         return True if goal == 0 else False
 
+    def canJump_dp(self, nums: List[int]) -> bool:
+        n = len(nums)
+        # dp[i]: can jump to last index at position i
+        dp = [False] * n
+        dp[n - 1] = True
+        for i in range(n - 2, -1, -1):
+            if nums[i] <= 0:
+                # can never jump to last at i
+                dp[i] = False
+                continue
+
+            reachable = i + nums[i]
+            for j in range(i + 1, reachable + 1):
+                if j < n and dp[j]:
+                    # can jump to j and then to last
+                    dp[i] = True
+        return dp[0]
+
+    def canJump_max_reach(self, nums: List[int]) -> bool:
+        reach = 0
+        for i in range(0, len(nums)):
+            if reach < i:
+                return False
+            reach = max(reach, i + nums[i])
+
+        return True
+
 
 class ProblemTest(unittest.TestCase):
 
@@ -22,13 +49,17 @@ class ProblemTest(unittest.TestCase):
         # case 1
         nums = [2, 3, 1, 1, 4]
         expected = True
-        ret = solution.canJump(nums)
+        # ret = solution.canJump(nums)
+        # ret = solution.canJump_dp(nums)
+        ret = solution.canJump_max_reach(nums)
         self.assertEqual(ret, expected)
 
         # case 2
         nums = [3, 2, 1, 0, 4]
         expected = False
-        ret = solution.canJump(nums)
+        # ret = solution.canJump(nums)
+        # ret = solution.canJump_dp(nums)
+        ret = solution.canJump_max_reach(nums)
         self.assertEqual(ret, expected)
 
 
