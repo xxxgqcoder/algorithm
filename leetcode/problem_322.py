0bc9ceb3c9ae6719ae189ab36330c25769c51690
xxxgqcoder <xxxgqcoder@gmail.com>
2025-06-25 16:25:34 +0800

    problem 322 bfs

--- leetcode/problem_322.py
+++ leetcode/problem_322.py
@@ -24,6 +24,34 @@ class Solution:
 
         return dp[amount]
 
+    def coinChange_BFS(self, coins: List[int], amount: int) -> int:
+        from collections import deque
+
+        q = deque()
+        q.append(amount)
+        visited = {}
+        level = 0
+
+        coins = sorted(coins)
+        while len(q) > 0:
+            level_size = len(q)
+
+            while level_size > 0:
+                # enumerate current level
+                level_size -= 1
+                current_amt = q.popleft()
+                visited[current_amt] = True
+                if current_amt == 0:
+                    return level
+
+                for c in coins:
+                    next_amt = current_amt - c
+                    if not visited.get(next_amt, False) and next_amt >= 0:
+                        q.append(next_amt)
+            level += 1
+
+        return -1
+
 
 class ProblemTest(unittest.TestCase):
 
@@ -33,28 +61,32 @@ class ProblemTest(unittest.TestCase):
         coins = [1, 2, 5]
         amount = 11
         expected = 3
-        ret = solution.coinChange(coins, amount)
+        # ret = solution.coinChange(coins, amount)
+        ret = solution.coinChange_BFS(coins, amount)
         self.assertEqual(ret, expected)
 
         # case 2
         coins = [2]
         amount = 3
         expected = -1
-        ret = solution.coinChange(coins, amount)
+        # ret = solution.coinChange(coins, amount)
+        ret = solution.coinChange_BFS(coins, amount)
         self.assertEqual(ret, expected)
 
         # case 3
         coins = [1]
         amount = 0
         expected = 0
-        ret = solution.coinChange(coins, amount)
+        # ret = solution.coinChange(coins, amount)
+        ret = solution.coinChange_BFS(coins, amount)
         self.assertEqual(ret, expected)
 
         # case 4
         coins = [474, 83, 404, 3]
         amount = 264
         expected = 8
-        ret = solution.coinChange(coins, amount)
+        # ret = solution.coinChange(coins, amount)
+        ret = solution.coinChange_BFS(coins, amount)
         self.assertEqual(ret, expected)
 
 
